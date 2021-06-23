379e35ad7402b057fba31e1413b2c543f72163bc
Keerti Chaudhary <43823232+keerti2001@users.noreply.github.com>
2021-06-24 01:48:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,17 @@ const App = ()=> {
   const [articles ,setArticles]=useState([])
   const [term, setTerm] = useState('everything')
   const [isloading, setIsLoading] = useState(true)
-  var url='https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+term+'&api-key='+process.env.REACT_APP_ARTICLES_API_KEY;
+  //var url='https://api.nytimes.com/svc/search/v2/articlesearch.json?q='+term+'&api-key='+process.env.REACT_APP_ARTICLES_API_KEY;
+  
   useEffect(()=>{
     const fetchArticles =async ()=>{  
     try{
           
-          const res= await fetch(url)
+          const res= await fetch("https://webhose.io/nseFilter?token=acf07eda-af87-4cd7-a8a6-c4ecbeb3bce1&q="+term);
           const articles= await res.json()
           console.log(articles);
-          console.log(articles.response.docs)
-          setArticles(articles.response.docs)
+          console.log(articles.docs)
+          setArticles(articles.docs)
           setIsLoading(false)
           
        } catch(error){
@@ -39,23 +40,18 @@ const App = ()=> {
    {isloading? <h1 className="text-center mt-20 font-bold text-6xl">Loading....</h1> :
     <section className=" grid grid-cols-1 px-5 pt-10 pb-20">
     {articles.map((article) => {
-      const {abstract, headline:{main}, byline:{original},
-    lead_paragraph, news_desk, section_name,
-    web_url, _id, word_count,} = article
+      const {article:{author,title,summary,uuid,url,social:{updated} }}= article
 
     return(
-      <article key={_id} className="bg-white py-10 px-5 my-4 rounded-lg lg:w-9/12 lg:mx-auto">
-        <h2 className="font-bold text-2xl mb-2 lg:text-4xl">{main}</h2>
-        <p>{abstract}</p>
-        <p>{lead_paragraph}</p>
+      <article key={uuid} className="bg-white py-10 px-5 my-4 rounded-lg lg:w-9/12 lg:mx-auto">
+        <h2 className="font-bold text-2xl mb-2 lg:text-4xl">{title}</h2>
+        <p>{summary}</p>
         
         <ul className="my-4">
-          <li>{original}</li>
-          <li><span className="font-bold">News Desk: </span>{news_desk}</li>
-          <li><span className="font-bold">Section Name : </span>{section_name}</li>
-          <li><span className="font-bold">Word Count : </span>{word_count}</li>
+          <li>By: {author}</li>
+          <li><span className="font-bold">updated : </span>{updated}</li>
         </ul>
-        <a href={web_url} target="_blank" className="underline">Web Resource</a>
+        <a href={url}className="underline">Web Resource</a>
         </article>
     )
 
